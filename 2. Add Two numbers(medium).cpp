d36baddf12ae4343b1479c71e1d50d9c47844b8a
Shalini S <115527635+ShaliniSJ@users.noreply.github.com>
2023-01-02 23:15:44 +0530

    Create 2. Add Two numbers(medium).cpp

---
+++ 2. Add Two numbers(medium).cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
+        ListNode* sumnode = new ListNode(0);
+        ListNode* head = sumnode;
+        int carry = 0;
+        while(l1 && l2){
+            int value = l1->val + l2->val + carry;
+            carry = value/10;
+            sumnode ->next = new ListNode(value%10);
+            sumnode = sumnode->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        while(l1){
+            int value = l1->val + carry;
+            carry = value/10;
+            sumnode ->next = new ListNode(value%10);
+            sumnode = sumnode->next;
+            l1 = l1->next;
+        }
+        while(l2){
+            int value = l2->val + carry;
+            carry = value/10;
+            sumnode ->next = new ListNode(value%10);
+            sumnode = sumnode->next;
+            l2 = l2->next;
+        }
+        if (carry != 0) {sumnode->next = new ListNode(carry);
+        }
+        return head->next;}
+};
